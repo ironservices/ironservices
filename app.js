b00a2f2a864518c903e338789b2bbc6821a07dae
Vitor Pavan <vipavani@hotmail.com>
2020-09-08 10:53:30 +0200

    inicial dependencies

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const bodyParser = require("body-parser");
+const favicon = require("serve-favicon");
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const flash = require("connect-flash");
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
